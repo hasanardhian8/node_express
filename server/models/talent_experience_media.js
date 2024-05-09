955885bc91201df04b0810e634e2f98724aea0bf
hasanardhian8 <hasanardhian11@gmail.com>
2024-05-09 22:09:05 +0700

    model

--- server/models/talent_experience_media.js
+++ server/models/talent_experience_media.js
@@ -1,44 +1,42 @@
-import _sequelize from 'sequelize';
-const { Model, Sequelize } = _sequelize;
-
-export default class talent_experience_media extends Model {
-  static init(sequelize, DataTypes) {
-  return super.init({
-    teme_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    teme_url_link: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    teme_media_upload: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    teme_taex_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'talent_experience',
-        key: 'taex_id'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'talent_experience_media',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
-      {
-        name: "teme_id_pk",
-        unique: true,
-        fields: [
-          { name: "teme_id" },
-        ]
+module.exports = (sequelize, DataTypes) => {
+  const Teme = sequelize.define(
+    "talent_experience_media",
+    {
+      teme_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
-  }
-}
+      teme_url_link: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      teme_media_upload: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      teme_taex_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "talent_experience",
+          key: "taex_id",
+        },
+      },
+    },
+    {
+      sequelize,
+      tableName: "talent_experience_media",
+      schema: "public",
+      timestamps: false,
+      indexes: [
+        {
+          name: "teme_id_pk",
+          unique: true,
+          fields: [{ name: "teme_id" }],
+        },
+      ],
+    }
+  );
+  return Teme;
+};
--- server/models/talent_timeline.js
+++ server/models/talent_timeline.js
@@ -1,49 +1,47 @@
-import _sequelize from 'sequelize';
-const { Model, Sequelize } = _sequelize;
-
-export default class talent_timeline extends Model {
-  static init(sequelize, DataTypes) {
-  return super.init({
-    tati_id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    tati_date: {
-      type: DataTypes.DATEONLY,
-      allowNull: true
-    },
-    tati_tale_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'talent',
-        key: 'tale_id'
-      }
-    },
-    tati_timeline_name: {
-      type: DataTypes.STRING(25),
-      allowNull: true,
-      references: {
-        model: 'time_line',
-        key: 'timeline_name'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'talent_timeline',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
-      {
-        name: "tati_id_pk",
-        unique: true,
-        fields: [
-          { name: "tati_id" },
-        ]
+module.exports = (sequelize, DataTypes) => {
+  const Tati = sequelize.define(
+    "talent_timeline",
+    {
+      tati_id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
-  }
-}
+      tati_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+      tati_tale_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "talent",
+          key: "tale_id",
+        },
+      },
+      tati_timeline_name: {
+        type: DataTypes.STRING(25),
+        allowNull: true,
+        references: {
+          model: "time_line",
+          key: "timeline_name",
+        },
+      },
+    },
+    {
+      sequelize,
+      tableName: "talent_timeline",
+      schema: "public",
+      timestamps: false,
+      indexes: [
+        {
+          name: "tati_id_pk",
+          unique: true,
+          fields: [{ name: "tati_id" }],
+        },
+      ],
+    }
+  );
+  return Tati;
+};
